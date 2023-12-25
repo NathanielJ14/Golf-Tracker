514016655cc39bcf380f12def7dfb4ddc7d12d48
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-25 11:04:59 -0500

    Create home page

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -3,25 +3,16 @@ import '../../public/stylesheets/home.css';
 
 const Home = () => {
     return (
-        <div className="d-flex text-center text-white">
-            <div className="cover-container d-flex w-100 h-100 p-4 mx-auto flex-column">
-                <header className="mb-5">
-                    <div>
-                        <nav className="nav nav-masthead justify-content-center float-md-right">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            <a className="nav-link" href="/dashboard">Dashboard</a>
-                            <a className="nav-link" href="/newgame">New Game</a>
-                            <a className="nav-link" href="/login">Login</a>
-                            <a className="nav-link" href="/register">Register</a>
-                        </nav>
-                    </div>
-                </header>
-
+        <div className="d-flex text-center text-white background">
+            <div className="container d-flex w-100 h-100 p-4 mx-auto flex-column">
                 <main className="px-3 mt-5 mb-5">
-                    <h1>Golf Tracker</h1>
-                    <p className="lead"> Welcome to Golf Tracker! <br /> Here you can see created campground as well as create your own. You can also leave reviews on other campgrounds.</p>
-                    <a href="/register" className="btn btn-lg font-weight-bold button m-5">Register</a>
-                    <a href="/login" className="btn btn-lg font-weight-bold button m-5">Login</a>
+                    <h1 className='mt-5'>Golf Tracker</h1>
+                    <p className="lead"> Welcome to Golf Tracker! <br /> Create an account or login to track your games, scores and more. </p>
+                    <div className='account'>
+                        <a href="/register" className="btn btn-lg font-weight-bold button mt-5 mb-3">Create an account</a>
+                        <p className='fs-4'>Or</p>
+                        <a href="/login" className="btn btn-lg font-weight-bold button ">Login</a>
+                    </div>
                 </main>
             </div>
         </div>
