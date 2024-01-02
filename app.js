6124fb6064541623e92cfd6036bd304ff2a615f5
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-01-02 18:51:43 -0500

    Change db url

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const userRoutes = require('./routes/users');
 const gameRoutes = require('./routes/games');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/golf-tracker';
+const dbUrl = process.env.DB_URL;
+// 'mongodb://localhost:27017/golf-tracker'
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
