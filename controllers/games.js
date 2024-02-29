ddbcdc140977e904222234139efc2d62f0305914
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-02-29 12:12:48 -0500

    Add comments to controllers

--- controllers/games.js
+++ controllers/games.js
@@ -1,3 +1,4 @@
+//Acquire game model
 const Game = require('../models/game');
 
 module.exports.index = async (req, res) => {
@@ -18,10 +19,12 @@ module.exports.index = async (req, res) => {
     }
 };
 
+//Render create new game form
 module.exports.renderNewForm = (req, res) => {
     res.render('games/new');
 }
 
+//Create new game
 module.exports.createGame = async (req, res, next) => {
     const game = new Game(req.body.game);
     game.author = req.user._id;
@@ -30,6 +33,7 @@ module.exports.createGame = async (req, res, next) => {
     res.redirect(`/games/${game._id}`);
 }
 
+//Finding single game and rendering show game page
 module.exports.showGame = async (req, res) => {
     const game = await Game.findById(req.params.id);
 
@@ -40,7 +44,9 @@ module.exports.showGame = async (req, res) => {
     res.render('games/show', { game });
 }
 
+//Render edit form page
 module.exports.renderEditForm = async (req, res) => {
+    //Looking for users game
     const { id } = req.params;
     const game = await Game.findById(id);
     if (!game) {
@@ -50,16 +56,20 @@ module.exports.renderEditForm = async (req, res) => {
     res.render('games/edit', { game });
 }
 
+//Update users game
 module.exports.updateGame = async (req, res) => {
     const { id } = req.params;
+    //Update with new info
     const game = await Game.findByIdAndUpdate(id, { ...req.body.game });
     await game.save();
     req.flash('success', 'Successfully updated your game!');
     res.redirect(`/games/${game._id}`);
 }
 
+//Delete users game
 module.exports.deleteGame = async (req, res) => {
     const { id } = req.params;
+    //Find game by game id and delete
     await Game.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted your game!');
     res.redirect('/games');
--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,18 @@
+//Acquire user model
 const User = require('../models/user');
 
+//Render register user page
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 }
 
+//Register user
 module.exports.register = async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
+        //Login user after creating an account
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash("success", "Welcome to Golf Tracker!");
@@ -20,10 +24,12 @@ module.exports.register = async (req, res, next) => {
     }
 }
 
+//Render login page
 module.exports.renderLogin = (req, res) => {
     res.render('users/login');
 }
 
+//Login user
 module.exports.login = (req, res) => {
     req.flash('success', 'Welcome Back!');
     const redirectUrl = res.locals.returnTo || '/games';
@@ -31,6 +37,7 @@ module.exports.login = (req, res) => {
     res.redirect(redirectUrl);
 }
 
+//Logout user
 module.exports.logout = (req, res, next) => {
     req.logout(function (err) {
         if (err) {
